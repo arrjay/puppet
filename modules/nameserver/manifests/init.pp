9e7007737bfc0a1e76324654d72b49caea9c7d04
RJ Bergeron <rbergero@gmail.com>
2013-11-10 14:33:11 -0500

    Unwound all the views in to hiera...this is clunky as hell.

--- modules/nameserver/manifests/init.pp
+++ modules/nameserver/manifests/init.pp
@@ -5,16 +5,22 @@ class nameserver {
       #$pkg          = "net/bind9"		# package name for named
       $cfg          = "/etc/namedb/named.conf"	# path to named config
       $dir          = "/etc/namedb"		# path for auxiliary named files	
+      $wrkdir       = "/etc/namedb/working"	# path for transient named data
+      $pidfile      = "/var/run/named/pid"	# path to pid-keeping file
+      $dumpfile     = "/var/dump/named_dump.db" # path to named db dump file
+      $statsfile    = "/var/stats/named.stats"	# path to named statistics tracking file
       $svc          = "named"			# service hook for named	
       $group        = "bind"			# named ownership group
       $owner        = "bind"			# named ownsership user
       $_chrootref   = $dir			# where named sees data internal to chroot
       $_named_ddirs = [
+        "/etc/namedb/slave",			# slave zone storage
         "/var/named/var/named/dynamic",		# a grouping of directories (to resolve a full path)
         "/var/named/var/named",			#  of where named keeps dynamic data.
       ]
       $_named_rdirs = [
         "/var/named/var", 			# the upper half of the named path - to be root owned
+        "/etc/namedb/master",			# actually the directory to pull master zone files from
       ]
     }
   }
@@ -27,6 +33,10 @@ class nameserver {
   file {$_named_rdirs:
     ensure => directory,
     owner  => 'root',
+  }
+  file {$wrkdir:
+    ensure => directory,
+    owner  => $owner,
   }
 
   # to be called if the actual named config changes
@@ -52,18 +62,12 @@ class nameserver {
     notify => Exec["reload named"],
   }
 
-  file { "$cfg":
-    ensure => present,
-    mode   => '0644',
-    owner  => 'root',
-    group  => 0,
-  }
-
   # collate default zone data, zone defaults
   # zones are defined outside the nameserver params, so you can share zones that way.
   $zonedefaults = hiera_hash('zonedefaults')
   $zonedata = hiera('zonedata')
   $params = hiera_hash('nameserver')
+  $nsgroup = hiera_hash('nsgroup')
 
   # we do not auto-hook zonefile to named config.
   define zonefile (
@@ -77,6 +81,7 @@ class nameserver {
     $nxttl    = $nameserver::zonedata[$title]['nxttl'],
     $copy     = $nameserver::zonedata[$title]['copy'],
     $domain   = $nameserver::params['zones'][$title]['ptrdomain'],
+    $type     = $nameserver::zonedata[$title]['type'],
   ) {
     # I'm not entirely sure why puppet needed hand-holding to look this one up
     $_tvar = $nameserver::params['zones'][$title]['template']
@@ -98,9 +103,10 @@ class nameserver {
     if $retry == undef { $_retry = $nameserver::zonedefaults['retry'] } else { $_retry = $retry }
     if $expiry == undef { $_expiry = $nameserver::zonedefaults['expiry'] } else { $_expiry = $expiry }
     if $nxttl == undef { $_nxttl = $nameserver::zonedefaults['nxttl'] } else { $_nxttl = $nxttl }
-    file { "$nameserver::dir/$title.db":
+    if $type == undef { $_type = master } else { $_type = $type }
+    file { "$nameserver::dir/$_type/$title.db":
       ensure  => present,
-      path    => "$nameserver::dir/$title.db",
+      path    => "$nameserver::dir/$_type/$title.db",
       content => template($_template),
       mode    => '0644',
       owner   => 'root',
@@ -111,7 +117,6 @@ class nameserver {
 
   # Fetch zones from server params if we can
   $zones = keys($params['zones'])
-  notice($zones)
   zonefile{$zones:}
 
   # inc is a subset of the nameserver config file, restart named if these change
@@ -135,8 +140,18 @@ class nameserver {
     inc{$incs:}
   }
 
-  # Fetch views from server params now
-  #$views = keys($params['views'])
+  # Fetch server params now
+  $addresses = keys($params['addresses'])
+  $views = keys($params['views'])
+
+  # Oh hey...write the named config.
+  file { "$cfg":
+    ensure  => present,
+    mode    => '0644',
+    owner   => 'root',
+    group   => 0,
+    content => template("nameserver/named.conf.erb"),
+  }
 
   # start named
   service { "$svc": }
