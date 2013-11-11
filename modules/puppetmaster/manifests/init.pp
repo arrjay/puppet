b0ad8aee1979ee7e90fa771afdd5978a66244ea0
RJ Bergeron <rbergero@gmail.com>
2013-11-11 11:13:07 -0500

    added deps for gem provider on CentOS

--- modules/puppetmaster/manifests/init.pp
+++ modules/puppetmaster/manifests/init.pp
@@ -1,5 +1,12 @@
 # Stuff useful for a puppetmaster
 class puppetmaster {
+  case $::operatingsystem {
+    'CentOS': {
+      package { ['ruby-devel', 'gcc']:
+        ensure => 'installed',
+      }
+    }
+  }
   package { 'hiera-gpg':
     ensure   => 'latest',
     provider => 'gem',
