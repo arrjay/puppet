6c9123086dca1a24975273a61ae280744ab10182
RJ Bergeron <rbergero@gmail.com>
2013-11-11 01:11:17 -0500

    Quick shot at a NFS server module.

---
+++ modules/nfs/manifests/server.pp
@@ -0,0 +1,19 @@
+class nfs::server {
+  case $::operatingsystem {
+    'FreeBSD': {
+      $exports = '/etc/exports'
+      $nfsd    = 'nfsd'
+      $mountd  = 'mountd'
+      $lockd   = 'lockd'
+    }
+  }
+
+  file{$exports:
+    ensure => present,
+    owner  => root,
+    group  => 0,
+    mode   => 0744,
+  }
+
+  service { ["$nfsd", "$mountd", "$lockd"]: enable => true, ensure => "running", }
+}
