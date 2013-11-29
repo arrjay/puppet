bb74846ea71a1504b991e0a1d70a940c6f9d59b4
RJ Bergeron <rbergero@gmail.com>
2013-11-29 18:59:48 -0500

    Now, let's graph something! TODO: Indices

---
+++ modules/graphing/lib/facter/targets.rb
@@ -0,0 +1,27 @@
+# targets.rb
+
+# try to figure out what we should be graphing. currently a nasty mess as I guess at rrd graph dirs.
+
+rrd_dirs = ['/var/lib/mrtg']
+
+graph_targets = []
+
+Facter.add("graph_targets") do
+  setcode do
+    rrd_dirs.each do |dir|
+    # is this a directory?
+     if FileTest.directory?(dir)
+       Dir.foreach(dir) do |dent|
+         # skip . files
+         next if dent =~ /^\./
+         # we also dump mrtg *files* in here. only list the directories.
+         if FileTest.directory?("#{dir}/#{dent}")
+           graph_targets.push(dent)
+         end
+       end
+     end
+    end
+    # flatten the array for puppet handoff.
+    graph_targets.join(",")
+  end
+end
--- modules/graphing/manifests/init.pp
+++ modules/graphing/manifests/init.pp
@@ -7,7 +7,13 @@ class graphing (
   $package = hiera('graphing::packages')	# notably, our graph scripts use 'declare -A' and thus require bash 4.x
 						# however, there is no good way in puppet to specify that! ask for the latest and hope.
 
+  include crontask				# we need to lay down monitoring scripts :)
+
+  # install packages we desire
   package {$package: ensure => latest }
+
+  # fish the mrtg data dir out for templates
+  $mrtgdir = $mrtg::dir
 
   # we use an exec block here to create any parents, rather than a file block. eeeeeh.
   exec {"/bin/mkdir -p $webdir":
@@ -25,5 +31,34 @@ class graphing (
   # suck in nginx and configure that now :)
   class {'httpd::nginx':
     sites => { "$::hostname" => { port => '80', locations => { '/' => { root => $webdir, }, }, }, },
+  }
+
+  # There should be a good way to ask puppet if I defined a monitoring task.
+  # I have no idea what that would be, though :)
+  # For the time being, work off the existence of previously defined mrtg dirs via our fact
+  # this means you need to run puppet at least twice.
+  if ($::graph_targets) {
+    $graph_targets = split($::graph_targets, ',')
+    if "ups" in $graph_targets {
+      file {"$webdir/ups":
+        ensure => directory,
+        owner  => root,
+        group  => 0,
+        mode   => 0755,
+      }
+
+      file {"$crontask::dir/rrdrender-ups.sh":
+        owner   => root,
+        group   => 0,
+        content => template("graphing/rrdrender-ups.sh.erb"), 
+        mode    => '0755',
+      }
+
+      cron {"rrdrender-ups":
+        command => "$crontask::dir/rrdrender-ups.sh",
+        user    => root,
+        minute  => "*/5",
+      }
+    }
   }
 }
