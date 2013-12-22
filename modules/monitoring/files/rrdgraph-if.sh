ce8ce64d8bd0838adaa00b8b9f808a516ab091fb
RJ Bergeron <rbergero@gmail.com>
2013-12-21 21:39:06 -0500

    Cron paths are stupid.

--- modules/monitoring/files/rrdgraph-if.sh
+++ modules/monitoring/files/rrdgraph-if.sh
@@ -4,6 +4,8 @@ outdir=$1
 
 # theory of operation: first we get a list of interfaces, then for each interface, we maintain an rrd.
 
+PATH=${PATH}:/sbin
+
 # RRAs to record, if we create an RRD
 rras="RRA:AVERAGE:0.5:1:1200 RRA:MIN:0.5:12:2400 RRA:MAX:0.5:12:2400 RRA:AVERAGE:0.5:12:2400"
 
