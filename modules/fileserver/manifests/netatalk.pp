84caf40b3f30e5cf7431fba87962dba50f19ad45
RJ Bergeron <rbergero@gmail.com>
2014-01-01 23:24:58 -0500

    Teach netatalk the ways of puppet-concat too.

--- modules/fileserver/manifests/netatalk.pp
+++ modules/fileserver/manifests/netatalk.pp
@@ -1,4 +1,6 @@
 class fileserver::netatalk(
+  $homeserver = hiera('fileserver::netatalk_homeserver',false),
+  $homepath   = hiera('fileserver::netatalk_homepath',undef),
 ) {
   # we call ldapclient directly so we can fish binding variables out of it. we need those.
   require ldapclient
@@ -8,10 +10,13 @@ class fileserver::netatalk(
   $groupbase  = $ldapclient::nss_group_base
   $ldapserver = $ldapclient::server
 
+  $svccmd     = hiera('service')
+
   case $::operatingsystem {
     'FreeBSD': {
       $atalk_pkg = "net/netatalk3"
       $afp_conf  = "/usr/local/etc/afp.conf"
+      $service   = "netatalk"
       # we need krb5 UAM support, so...
       require freebsd::portupgrade
       # install prereqs via packages, *then* build it.
@@ -32,11 +37,41 @@ class fileserver::netatalk(
     }
   }
 
-  file{"$afp_conf":
+  # reload handler
+  exec{"$svccmd $service restart":
+    refreshonly => true,
+    subscribe   => Concat[$afp_conf],
+  }
+
+  # write out a config
+  concat{"$afp_conf":
     owner   => root,
     group   => 0,
     mode    => 0644,
-    ensure  => present,
-    content => template("fileserver/afp.conf.erb"),
   }
+
+  concat::fragment{'base netatalk/afp config':
+    target  => $afp_conf,
+    content => template("fileserver/afp.conf.erb"),
+    order   => 00,
+  }
+
+  # configure/start netatalk
+  define share(
+    $share         = $title,
+    $basedir_regex = undef,
+  ) {
+    concat::fragment{"afp share: $share":
+      target  => $fileserver::netatalk::afp_conf,
+      content => template("fileserver/afp.share.conf.erb"),
+      order   => 10,
+    }
+  }
+
+  if $homeserver {
+    share{"Homes": basedir_regex => $homepath }
+  }
+
+  # configure netatalk to start/start it
+  service{"$service": enable => true, ensure => running }
 }
