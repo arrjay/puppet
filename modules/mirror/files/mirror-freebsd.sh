d63c03f90f98a8868a6654b2b8ff559e5ef6df12
RJ Bergeron <rbergero@gmail.com>
2014-01-22 15:20:24 -0500

    Provide a FreeBSD 10.0 mirror. Sure.

--- modules/mirror/files/mirror-freebsd.sh
+++ modules/mirror/files/mirror-freebsd.sh
@@ -10,6 +10,6 @@ for ver in ${FREEBSD_VERSIONS} ; do
   for arch in ${FREEBSD_ARCHITECTURES} ; do
     mkdir -p ${TOPLEVEL}/FreeBSD/releases/${arch}/${ver}/
     # ncftp makes ${ver}... or rather, the last directory specified.
-    ncftpget -R -f ${home}ncftp/freebsd.cfg ${TOPLEVEL}/FreeBSD/releases/${arch}/ pub/FreeBSD/releases/${arch}/${ver}/
+    ncftpget -R -f ${home}/ncftp/freebsd.cfg ${TOPLEVEL}/FreeBSD/releases/${arch}/ pub/FreeBSD/releases/${arch}/${ver}/
   done
 done
