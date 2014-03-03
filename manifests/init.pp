5723755e086a4366b4dce916e79226c16b1e79d7
RJ Bergeron <rbergero@gmail.com>
2014-03-02 20:25:59 -0500

    Added management of dnsmasq for virtual server platforms

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,7 +1,21 @@
 # common things for our vm servers (be they Xen, Virtualbox, or KVM)
 class vmserver (
-  $packages = hiera('vmserver::packages')
+  $packages = hiera('vmserver::packages'),
+  $dnsmasq_conf = hiera('dnsmasq::configfile'),
+  $dnsmasq_svcname = hiera('dnsmasq::svcname'),
+  $svccmd = hiera('service'),
 ) {
   include resolvconf
   package{$packages: ensure => installed}
+  file{$dnsmasq_conf:
+    owner	=> root,
+    group	=> 0,
+    mode	=> 0644,
+    ensure	=> present,
+    content	=> template("vmserver/dnsmasq.conf.erb"),
+    notify	=> Exec["$svccmd $dnsmasq_svcname restart"],
+  }
+  exec{"$svccmd $dnsmasq_svcname restart":
+    refreshonly => true,
+  }
 }
