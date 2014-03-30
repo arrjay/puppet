43769f0c0d389902b698c87e61d9f569a0338df5
RJ Bergeron <rbergero@gmail.com>
2014-03-30 00:40:58 -0400

    Make zpools a *little* safer here.

--- modules/zpool/manifests/init.pp
+++ modules/zpool/manifests/init.pp
@@ -1,6 +1,8 @@
 class zpool (
   $pools = hiera('zpools'),
+  $noop  = hiera('zpool::noop',true),
 ) {
+  $defaults = { noop => $noop }
   # just a basic class around the puppet zpool objects, so we can build from a base.
-  create_resources( zpool, $pools )
+  create_resources( zpool, $pools, $defaults )
 }
