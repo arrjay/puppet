742c068c4dea216c71ef9a34ea7b8a4bd5ab9645
RJ Bergeron <rbergero@gmail.com>
2014-04-02 22:17:18 -0400

    Add fx.ARCS for ip22 netbooting - linux fdisk is *awful*, so we use that to lay a disk out.

--- modules/netboot/manifests/ip2x_common.pp
+++ modules/netboot/manifests/ip2x_common.pp
@@ -1,5 +1,14 @@
 class netboot::ip2x_common (
+  $fxarcs_src = hiera('netboot::uri::fxarcs'),
 ) {
+  include inetd::tftpd
   # SGI IP22 machines often try to treat UDP port ranges as a signed integer, so...
   class{"tuning::freebsd": portrange_last => '32767'}
+
+  # Go get the IRIX partitioner.
+  exec{"get fx.ARCS":
+    command => "/usr/bin/fetch $fxarcs_src",
+    cwd => $inetd::tftpd::tftproot,
+    creates => "$inetd::tftpd::tftproot/fx.ARCS",
+  }
 }
