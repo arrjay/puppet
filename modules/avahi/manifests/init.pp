05a1acaab7eec3ac3558671364836d784d4eb15a
RJ Bergeron <rbergero@gmail.com>
2014-07-22 23:29:30 -0400

    Some work on adding netatalk2 support. A little noodly. (And doesn't cover that it needs a custom fbsd kernel!)

--- modules/avahi/manifests/init.pp
+++ modules/avahi/manifests/init.pp
@@ -8,7 +8,13 @@ class avahi {
   }
   case $::operatingsystem {
     'FreeBSD': {
+       if ($pkgng_enabled) {
+         # install avahi metaport now
+         package{ 'net/avahi': ensure => installed }
+       }
        # kick DBUS, then avahi
+       package{ 'devel/dbus': ensure => installed }
+       
        avahi::fbstartavahi { 'avahi-daemon': }
        service{"dbus": enable => true, ensure => running, } ~> service{"avahi-daemon": enable => true, } ~> Fbstartavahi[ 'avahi-daemon' ]
     }
---
+++ modules/fileserver/manifests/netatalk2.pp
@@ -0,0 +1,94 @@
+class fileserver::netatalk2(
+  $homeserver = hiera('fileserver::netatalk_homeserver',false),
+  $homepath   = hiera('fileserver::netatalk_homepath',undef),
+) {
+  # die if you called the 'default' netatalk module
+  if defined(Class['fileserver::netatalk']) {
+    fail("Can not run two netatalk servers at once")
+  }
+  # we call ldapclient directly so we can fish binding variables out of it. we need those.
+  require ldapclient
+  # we call krbclient because we might just talk that
+  require krbclient
+  $ldapdn     = $ldapclient::binddn
+  $ldappw     = $ldapclient::bindpw
+  $userbase   = $ldapclient::nss_passwd_base
+  $groupbase  = $ldapclient::nss_group_base
+  $ldapserver = $ldapclient::server
+
+  $svccmd     = hiera('service')
+
+  case $::operatingsystem {
+    'FreeBSD': {
+      # go pick up portsnap here - we need it.
+      require freebsd::portsnap
+
+      $atalk_pkg = "net/netatalk"
+      $afp_conf  = "/usr/local/etc/afp.conf"
+      $service   = "netatalk"
+      # we need krb5 UAM support, so...
+      require freebsd::portupgrade
+      # install prereqs via packages, *then* build it.
+      package{[
+        "converters/libiconv",
+        "databases/db46",
+        "devel/gmake",
+        "devel/pkgconf",
+        "lang/perl5.16",
+        "net/avahi-app",
+        "security/libgcrypt",
+        "net/openslp",
+      ]: ensure => installed } ~> package{$atalk_pkg: ensure => installed, provider => 'portupgrade' }
+      # we need avahi for netatalk to properly announce
+      require avahi
+    }
+  }
+
+  # reload handler
+  exec{"$svccmd $service restart":
+    refreshonly => true,
+    subscribe   => Concat[$afp_conf],
+  }
+
+  # write out a config
+  concat{"$afp_conf":
+    owner   => root,
+    group   => 0,
+    mode    => 0644,
+  }
+
+  concat::fragment{'base netatalk/afp config':
+    target  => $afp_conf,
+    content => template("fileserver/afp.conf.erb"),
+    order   => 00,
+  }
+
+  # configure/start netatalk
+  define share(
+    $share         = $title,
+    $basedir_regex = undef,
+  ) {
+    concat::fragment{"afp share: $share":
+      target  => $fileserver::netatalk::afp_conf,
+      content => template("fileserver/afp.share.conf.erb"),
+      order   => 10,
+    }
+  }
+
+  if $homeserver {
+    share{"Homes": basedir_regex => $homepath }
+  }
+
+  # configure netatalk to start/start it
+  service{"$service": enable => true, ensure => running }
+
+  # install pam config if missing
+  file{"/etc/pam.d/netatalk":
+    replace => no,
+    ensure  => present,
+    mode    => 0644,
+    owner   => root,
+    group   => 0,
+    source  => "puppet:///modules/fileserver/pam_d_netatalk",
+  }
+}
