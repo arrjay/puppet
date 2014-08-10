db8af271450d8e4004f266575d3406005a38c700
RJ Bergeron <rbergero@gmail.com>
2014-08-09 23:33:31 -0400

    add support for installing zfsonlinuz when asked to define a zpool here (requires: c6 host)

--- modules/zpool/manifests/init.pp
+++ modules/zpool/manifests/init.pp
@@ -2,6 +2,29 @@ class zpool (
   $pools = hiera('zpools'),
   $noop  = hiera('zpool::noop',true),
 ) {
+  # if this is a linux variant, pull in the zfs module now.
+  case $::osfamily {
+    'RedHat' : {
+      yumrepo {'zfs':
+        enabled  => '1',
+        gpgcheck => '1',
+        baseurl  => 'http://archive.zfsonlinux.org/epel/6/$basearch',
+        before   => Package['zfs'],
+      }
+      file {'/etc/pki/rpm-gpg/RPM-GPG-KEY-zfsonlinux':
+        owner  => root,
+        group  => 0,
+        mode   => 0644,
+        source => 'puppet:///modules/zpool/RPM-GPG-KEY-zfsonlinux',
+        before => Exec['add zfs gpgkey'],
+      }
+      exec{'add zfs gpgkey':
+        command => "/bin/rpm --import /etc/pki/rpm-gpg/RPM-GPG-KEY-zfsonlinux",
+        unless => "/bin/rpm -qi gpg-pubkey-f14ab620-514b76b7"
+      }
+      package{'zfs': ensure => present}
+    }
+  }
   $defaults = { noop => $noop }
   # just a basic class around the puppet zpool objects, so we can build from a base.
   create_resources( zpool, $pools, $defaults )
