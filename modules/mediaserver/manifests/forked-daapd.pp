747f3ab0e7761a0525ce3c54f74b905ac5a2e445
RJ Bergeron <rbergero@gmail.com>
2014-08-18 01:33:33 -0400

    utter erbification

--- modules/mediaserver/manifests/forked-daapd.pp
+++ modules/mediaserver/manifests/forked-daapd.pp
@@ -1,9 +1,24 @@
 class mediaserver::forked-daapd (
+  $servername = $::hostname,
+  $port = 3689,
+  $password = undef,
+  $podcast_dir = '_PODCASTS',
+  $audiobook_dir = '_AUDIOBOOKS',
+  $compilations_dir = '_COMPILATIONS',
+  $compilation_artist = 'Compilations',
+  $artwork_names = [],
+  $ignore_filetypes = [ '.db', '.ini', '.flac' ],
+  $filescan_disable = false,
+  $skip_transcode = [ 'alac', 'mp4a' ],
+  $force_transcode = [ 'ogg', 'flac' ],
 ) {
   # per-os variables
   case $::osfamily {
     'RedHat' : {
       $daapd_config = '/etc/forked-daapd.conf'
+      $daap_user = 'forked-daapd'
+      $forked_daapd_log = '/var/log/forked-daapd.log'
+      $forked_daapd_db = '/var/cache/forked-daapd/songs3.db'
     }
   }
 
