43ed1e5403b6ac84b6d0eeb74f017cacb948fed8
RJ Bergeron <rbergero@gmail.com>
2014-08-29 21:34:46 -0400

    fold nfs::server into fileserver::nfs...

--- modules/fileserver/manifests/nfs.pp
+++ modules/fileserver/manifests/nfs.pp
@@ -1,12 +1,31 @@
 class fileserver::nfs (
 ) {
   # kick a basic nfs server up
+  include fileserver
+  include portmap
+
   case $::osfamily {
     'RedHat' : {
       $packages = [ 'nfs-utils' ]
       $services = [ 'rpcbind', 'nfs-server' ]
+      $exports  = '/etc/exports'
+    }
+    'FreeBSD' : {
+      $exports  = '/etc/exports'
+      $services = [ 'nfsd', 'mountd', 'lockd' ]
     }
   }
-  package {$packages: ensure => installed}
+
+  file{$exports:
+    ensure => present,
+    owner  => root,
+    group  => 0,
+    mode   => 0644,
+  }
+
+  if $packages {
+    package {$packages: ensure => installed}
+  }
+
   service {$services: enable => true}
 }
