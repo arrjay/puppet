ee45ab320850a19fdab6bd293eab4dd27ac538e6
RJ Bergeron <rbergero@gmail.com>
2015-05-24 15:43:18 -0400

    clean up resolvconf handling, editing of dhclient.conf

--- modules/resolvconf/manifests/init.pp
+++ modules/resolvconf/manifests/init.pp
@@ -7,8 +7,8 @@ class resolvconf (
 ) {
   # this class mostly exists to blow up ubuntu's resolvconf idiocy
 
-  $servcount = size($nameservers)
-  $random = fqdn_rand($servcount,62467)
+  $ns_shuffled = fqdn_rotate($nameservers)
+  $ns_string = join($ns_shuffled, ",")
 
   file {$file:
     owner	=> root,
@@ -19,10 +19,15 @@ class resolvconf (
   }
 
   if ($manage_dhclient) {
-    augeas{"$dhclient_cfg: supersede nameservers"
-      changes => [
-        "set /files$dhclient_cfg/supersede/domain-name-servers = \"$nameservers\"",
-      ],
+    $dhcp_opt_string = "supersede domain-name-servers $ns_string;"
+    # augeas lens for dhcpd doesn't handle supersede - https://bugs.launchpad.net/ubuntu/+source/augeas/+bug/1193176
+    exec {"$dhclient_cfg: add 'supersede domain-name-servers' opt":
+      command => "echo '$dhcp_opt_string' >> $dhclient_cfg",
+      unless  => "grep -q '^supersede domain-name-servers' $dhclient_cfg",
+    }
+    exec {"$dhclient_cfg: reset 'supersede domain-name-servers' opt":
+      command => "perl -pi -e 's/supersede domain-name-servers.*/$dhcp_opt_string/' $dhclient_cfg",
+      unless  => "grep -q '^$dhcp_opt_string' $dhclient_cfg",
     }
   }
 }
