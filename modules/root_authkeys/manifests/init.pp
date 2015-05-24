c0f49c15253c7188e7fbd1ae0fe9a6252701b4f7
RJ Bergeron <rbergero@gmail.com>
2015-05-23 21:13:08 -0400

    stomp our authorized keys into all the things

---
+++ modules/root_authkeys/manifests/init.pp
@@ -0,0 +1,17 @@
+# stomp on .ssh/authorized_keys for the root user
+class root_authkeys 
+  $roothome = '/root',
+) {
+  file{"$roothome/.ssh":
+    ensure	=> directory,
+    mode	=> '0700',
+    owner	=> 0,
+  }
+
+  file{"$roothome/.ssh/authorized_keys":
+    ensure	=> present,
+    mode	=> '0644',
+    owner	=> 0,
+    source	=> 'puppet:///modules/root_authkeys/authorized_keys',
+  }
+}
