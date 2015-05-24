f7e520d4855ea51301f2de2e281bfe28bd06fedd
RJ Bergeron <rbergero@gmail.com>
2015-05-23 22:10:50 -0400

    stupid exec!

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,1 +1,8 @@
+# provide a default path for exec blocks here.
+Exec {
+  path => $osfamily ? {
+    default => "/usr/sbin:/sbin:/usr/bin:/bin:/usr/local/sbin:/usr/local/bin"
+  }
+}
+
 hiera_include('classes')
