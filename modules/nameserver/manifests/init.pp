e697febb2ab78c3a1af82e76cd45a479643e3210
RJ Bergeron <rbergero@gmail.com>
2015-05-25 12:35:48 -0400

    add nameserver class as a bridge from hiera to bind

---
+++ modules/nameserver/manifests/init.pp
@@ -0,0 +1,26 @@
+# wrap around the bind class to do exactly what we want.
+class nameserver (
+  $forwarders = [ '8.8.8.8', '8.8.4.4' ],
+  # puppet bug #20199
+  $views      = hiera_hash('nameserver::views')
+  $includes   = hiera_array('nameserver::includes')
+) {
+  class { bind: chroot => true }
+
+  # delete the non-chroot config on c7 to ensure we don't start non-chrooted named
+  case $::osfamily {
+   'RedHat': {
+     if versioncmp($operatingsystemmajrelease,"7") >= 0 {
+       file{'/etc/named.conf':
+         ensure => absent,
+       }
+     }
+   }
+  }
+
+  bind::server::conf { '/var/named/chroot/etc/named.conf':
+    forwarders => $forwarders,
+    views      => $views,
+    includes   => $includes,
+  }
+}
