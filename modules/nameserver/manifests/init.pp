63af241529f752c30bd836fa2772c8ee020f754e
RJ Bergeron <rbergeron@palantir.com>
2015-05-24 21:51:03 -0400

    try dealing with the zone list through hiera?

--- modules/nameserver/manifests/init.pp
+++ modules/nameserver/manifests/init.pp
@@ -2,10 +2,12 @@ class nameserver (
   $upstreams = [ '8.8.8.8', '8.8.4.4' ],
   $listen_ipv4 = '127.0.0.1',
   $listen_ipv6 = '::1',
+  $template,
 ) {
   # stand up an authoritative name server
   case $::osfamily {
     'RedHat' : {
+        $config   = '/var/named/chroot/etc/named.conf'
         $packages = [ 'bind-chroot' ]
       if versioncmp( $::operatingsystemrelease, "7") >= 0 {
         $service  = 'named-chroot'
@@ -15,5 +17,16 @@ class nameserver (
     }
   }
 
+  # template specific crap
+  $zones = hiera_hash("nameserver::zones")
+
   ensure_packages($packages)
+
+  file{$cfg:
+    ensure  => present,
+    content => template("nameserver/$template.conf.erb"),
+    mode    => "0640",
+    owner   => 0,
+    group   => "named",
+  }
 }
