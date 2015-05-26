332e6c15fca00aa98ef385ae36a9d6893b3cb27f
RJ Bergeron <rbergero@gmail.com>
2015-05-25 23:23:57 -0400

    oh look, bind itself deals with this key in a *third* way. hate? hate.

---
+++ modules/isc_key/manifests/named.pp
@@ -0,0 +1,18 @@
+define isc_key::named (
+  $name,
+  $path  = $title,
+  $owner = 0,
+  $group = 'named',
+  $mode  = '0640',
+) {
+  $keyname = $name
+  $keyhash = hiera("isc_key::$name")
+  $keydata = $keyhash[data]
+  $keytype = $keyhash[type]
+  file{$path:
+    owner   => $owner,
+    group   => $group,
+    mode    => $mode,
+    content => template('isc_key/named.erb')
+  }
+}
