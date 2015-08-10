357628c24fcfb6ac4cfa2df7e9aa1288a9125fb2
RJ Bergeron <rbergero@gmail.com>
2015-08-09 21:15:37 -0400

    first pass at a share handler

---
+++ modules/samba/manifests/share.pp
@@ -0,0 +1,24 @@
+define samba::share(
+  $share = $title,
+  $path,
+  $description = undef,
+  $browseable = true,
+  $printable = false,
+  $writeable = false,
+  $public = false,
+  $create_mask = undef,
+  $dir_mask = undef,
+  $hide_unreadable = false,
+) {
+  include samba
+
+  if $share == "global" {
+    fail('you may not have a samba share named global!')
+  }
+
+  concat::fragment{"$samba::config: [share:$share]":
+    target  => 'smb.conf',
+    content => template('samba/share.erb'),
+    order   => $samba::share_order,
+  }
+}
