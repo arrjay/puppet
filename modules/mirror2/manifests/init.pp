282611342a867d45c0612e2a0ea5844bca46b0dd
RJ Bergeron <rbergero@gmail.com>
2015-08-15 12:49:31 -0400

    oh, and add a note that you may need restorecon afterward anyway.

--- modules/mirror2/manifests/init.pp
+++ modules/mirror2/manifests/init.pp
@@ -7,5 +7,11 @@ class mirror2 (
     # this will force you to write out a selinux config decision and let hiera deal with that.
     $seconfig = hiera('selinux::mode')
     require selinux
+
+    # NOTE: this will configure the filecontext for all *new* files, you may still need restorecon -R on the mirror dir to clean up.
+    selinux::fcontext{'set-mirrors-httpd_t':
+      context  => 'httpd_sys_content_t',
+      pathname => "$dest(/.*)?",
+    }
   }
 }
