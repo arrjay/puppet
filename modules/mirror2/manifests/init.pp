a5c97fc111b319f665fb35005616c0c94a36f303
RJ Bergeron <rbergero@gmail.com>
2015-08-15 12:32:20 -0400

    start managing selinux for the mirror box. notably, don't turn it off...

--- modules/mirror2/manifests/init.pp
+++ modules/mirror2/manifests/init.pp
@@ -2,4 +2,10 @@ class mirror2 (
   $dest         = hiera('mirror2::destination'),
   $rsync_source = hiera('mirror2::rsync_source'),
 ) {
+  # if selinux is around, reset ALL THE FILE CONTEXTS here.
+  if $::selinux {
+    # this will force you to write out a selinux config decision and let hiera deal with that.
+    $seconfig = hiera('selinux::mode')
+    require selinux
+  }
 }
