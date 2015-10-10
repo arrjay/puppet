ca6f509020547208f2dd5e9de3f7dbe23e69ac93
RJ Bergeron <rbergero@gmail.com>
2015-10-09 22:37:37 -0400

    check for keys, hiera-eyaml gem

--- apply.sh
+++ apply.sh
@@ -2,15 +2,31 @@
 
 PUPPETVER=$(facter -p puppetversion)
 PUPPETARGS=""
+HIERA_CONFIG=""
 
-case PUPPETVER in
+if [ -f "./keys/private_key.pkcs7.pem" ] && [ -f "./keys/public_key.pkcs7.pem" ] ; then
+  GEMCHECK=$(gem list -i hiera-eyaml)
+  case ${GEMCHECK} in
+    true)
+      HIERA_CONFIG="./headless-e.yaml"
+      ;;
+    false)
+      echo "you don't have the hiera-eyaml gem, you should fix that."
+      exit 1
+      ;;
+  esac 
+else
+  HIERA_CONFIG="./headless.yaml"
+fi
+
+case ${PUPPETVER} in
   0*|1*|2*)
     # wow how did you get here?
     echo "unsupported puppet version ${PUPPETVER}"
     exit 1
     ;;
   3*)
-    PUPPETARGS="--parser=future"
+    PUPPETARGS="${PUPPETARGS} --parser=future"
     ;;
   *)
     # noop
@@ -22,4 +38,4 @@ if [ -z "${PUPPETVER}" ] ; then
   exit 1
 fi
 
-puppet apply ${PUPPETARGS} --hiera_config=./headless.yaml --modulepath ./modules ./manifests/site.pp
+puppet apply ${PUPPETARGS} --hiera_config="${HIERA_CONFIG}" --modulepath ./modules ./manifests/site.pp
