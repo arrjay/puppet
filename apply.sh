ffc3abc66f4ace3680b59d2be6d1590d6c3aa49d
RJ Bergeron <rbergero@gmail.com>
2015-10-09 22:18:37 -0400

    try supporting puppet3 and puppet4 in same apply.

--- apply.sh
+++ apply.sh
@@ -1,3 +1,25 @@
 #!/bin/sh
 
-puppet apply --parser=future --hiera_config=./headless.yaml --modulepath ./modules ./manifests/site.pp
+PUPPETVER=$(facter -p puppetversion)
+PUPPETARGS=""
+
+case PUPPETVER in
+  0*|1*|2*)
+    # wow how did you get here?
+    echo "unsupported puppet version ${PUPPETVER}"
+    exit 1
+    ;;
+  3*)
+    PUPPETARGS="--parser=future"
+    ;;
+  *)
+    # noop
+    ;;
+esac
+
+if [ -z "${PUPPETVER}" ] ; then
+  echo "I don't think you even have puppet. where's facter?"
+  exit 1
+fi
+
+puppet apply ${PUPPETARGS} --hiera_config=./headless.yaml --modulepath ./modules ./manifests/site.pp
